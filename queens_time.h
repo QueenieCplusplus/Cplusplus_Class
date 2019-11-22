291755aa681bc2772dfef622b6fffd063f01161d
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-11-23 07:55:55 +0800

    C++ header v1.0.2

--- queens_time.h
+++ queens_time.h
@@ -16,7 +16,15 @@ class Time
         Time(int h, int m = 0);
         void AddMin(int m);
         void AddHr(int h);
-        void ResetTime(int h = 0, int m = 0)     
+        void ResetTime(int h = 0, int m = 0) 
+
+        //常數定義
+        //...
+
+        //朋友關鍵字僅適用於類別宣告的函數原型
+        friend Time operator*(double m, const Time & t)
+            { return t * m; }
+        friend std::ostream & operator<<(std::ostream & os, const Time & t);
 };
 #endif
 
