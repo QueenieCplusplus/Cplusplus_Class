5a3332f844bfc9e15c0206320d46fc69c6f3f141
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-11-23 08:09:12 +0800

    Add files via upload

---
+++ show_queens_time.cpp
@@ -0,0 +1,23 @@
+// 2019, 11/23, 8:00, by Queenie
+
+#include "queens_time.h"
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    Time haha1123(5, 39);
+    Time heheSat(2, 30);
+    Time temp;
+
+    cout << "Queenie code C++ in Sat morning";
+    cout << haha1123 << "&" << heheSat << endl;
+
+    // member operators
+    temp = haha1123 * 1.5;
+    cout << temp << endl;
+
+    return 0;
+}
